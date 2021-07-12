69a90e47e0cab5f81cbf7661095bf994ed4f85ed
jose <pachonjcl@gmail.com>
2021-07-11 21:28:17 -0400

    fix circleA

--- components/circle2/CircleA.tsx
+++ components/circle2/CircleA.tsx
@@ -1,74 +1,119 @@
 import React, { useState, useEffect, useRef } from 'react'
 import * as d3 from 'd3'
-import Da from './data.json'
 import Fla from './flare.json'
 
 export default function CircleA() {
-    const ref = useRef<SVGSVGElement | null>(null)
-    const [data, setDat] = useState<any | null>(Fla)
-    var svg: any
-    //ESTE DATO ES EL PROBLEMA
-    var view=[470, 470, 960]
+    const ref = useRef<SVGSVGElement | null>(null);
+    const [data] = useState<any | null>(Fla);
+
+    const height = 960;
+    const width = 960;
+    var diameter = width;
+
     var margin = 20
-    var diameter: any
-    var nodes: any
-    var node: any
 
-    var circle: any
-    var focus: any
+    function draw() {
+        var svg = d3.select(ref.current)
 
-    function initial() {
+        svg
+            .style("width", width + 'px')
+            .style("height", height + 'px');
 
-        var g = svg.append("g").attr("transform", "translate(" + 1 + "," + 1 + ")");
+        var g = svg
+            .append("g")
+            .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
         var pack = d3.pack()
             .size([diameter - margin, diameter - margin])
+        var colorRange: any = ["hsl(152,80%,80%)", "hsl(228,30%,40%)"];
+        var d3Interpolation: any = d3.interpolateHcl;
+        var color = d3.scaleLinear()
+            .domain([-1, 5])
+            .range(colorRange)
+            .interpolate(d3Interpolation);
 
-        var rot = d3.hierarchy(data)
+        var root: any = d3.hierarchy(data)
             .sum(function (d) { return d.size; })
             .sort(function (a: any, b: any) { return b.value - a.value; })
-        focus = rot;
-        nodes = pack(rot).descendants();
-        circle = g.selectAll("circle")
+
+        var focus = root,
+            nodes = pack(root).descendants();
+        var view : any;
+
+        var circle = g.selectAll("circle")
             .data(nodes)
             .enter().append("circle")
-            .attr("class", function (d: any) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
-            .attr('cx', function (d: any) { return d.x })
-            .attr('cy', function (d: any) { return d.y })
-            .attr('r', function (d: any) { return d.r })
-            .attr('fill', 'transparent')
-            .attr('stroke', 'black')
-            .on("click", function (d: any) { if (focus !== d) zoom(d), d3.event.stopPropagation(); });
-        node = g.selectAll("circle,text");
+            .attr("class", function (d: any) {
+                return d.parent ? (d.children ? "node" : "node node--leaf") : "node node--root";
+            })
+            .style("fill", function(d) { return d.children ? color(d.depth) : null; })
+            .on("click", function(d) {
+                if (focus !== d) {
+                    zoom(d);
+                    var _d3: any = d3;
+                    var event: any = _d3.event;
+                    event.stopPropagation();
+                }
+            });
+            var text = g.selectAll("text")
+                .data(nodes)
+                .enter().append("text")
+                .attr("class", "label")
+                .style("fill-opacity", function(d) { return d.parent === root ? 1 : 0; })
+                .style("display", function(d) { return d.parent === root ? "inline" : "none"; })
+                .text(function(d: any) { return d.data.name; });
+            var node = g.selectAll("circle,text");
+
+            svg
+                .style("background", color(-1))
+                .on("click", function() { zoom(root); });
+
+            zoomTo([root.x, root.y, root.r * 2 + margin]);
+            
+            function zoom(d: any) {
+                var focus0 = focus; 
+                focus = d;
+                let v: any = view;
+                var _d3: any = d3;
+                var event: any = _d3.event;
+                var transition = d3.transition()
+                    .duration(event ? 1000 : 750)
+                    .tween("zoom", function (d) {
+                        var i = d3.interpolateZoom(v, [focus.x, focus.y, focus.r * 2 + margin]);
+                        return function (t) { zoomTo(i(t)); };
+                    });
+                transition.selectAll("text")
+                    .filter(function (d: any) { return d.parent === focus })
+                    .style("fill-opacity", function (d: any) { return d.parent === focus ? 1 : 0; })
+                    .on("start", function(d: any) {
+                        //if (d.parent === focus)
+                        //   this.style.display = "inline";
+                    })
+                    .on("end", function(d: any) {
+                        //if (d.parent !== focus)
+                        //   this.style.display = "none";
+                    });
+            }
+            function zoomTo(v: any) {
+                var k = diameter / v[2]; view = v;
+                node.attr("transform", function (d: any) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
+                circle.attr("r", function (d: any) { return d.r * k; });
+            }
 
     }
-    function zoom(d: any) {
-        var focus0 = focus; focus = d;
-        console.log('view=>', view, 'llego hasta aqui')
-        
-        var transition = d3.transition()
-            .duration(750)
-            .tween("zoom", function (d) {
-                var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
-                return function (t) { zoomTo(i(t)); };
-            });
-        transition.selectAll("text")
-            .filter(function (d: any) { return d.parent === focus })
-            .style("fill-opacity", function (d: any) { return d.parent === focus ? 1 : 0; })
-    }
-    function zoomTo(v: any) {
-        console.log('no llego aqui')
-        var k = diameter / v[2]; view = v;
-        node.attr("transform", function (d: any) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
-        circle.attr("r", function (d: any) { return d.r * k; });
-    }
     useEffect(() => {
-        svg = d3.select(ref.current)
-        diameter = +svg.attr("width")
-        initial()
+        draw()
     }, [])
+
+    const transition = () => {
+
+    }
+
     return (
         <div>
-            <svg ref={ref} width='960' height='960' />
+            <svg ref={ref} />
+            <button type="button" onClick={transition}>
+                Transition
+            </button>
         </div>
     )
 }
